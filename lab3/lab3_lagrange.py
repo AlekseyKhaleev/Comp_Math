60da902afed662d3aba1e3e50670a9283f53531d
Aleksey Khaleev <aleksey52nn@gmail.com>
2023-11-25 09:55:39 +0300

    lab3 done

--- lab3/lab3_lagrange.py
+++ lab3/lab3_lagrange.py
@@ -1,2 +1,80 @@
+import operator
+import warnings
+from functools import reduce
+from math import factorial
+
+from scipy.misc import derivative
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+def lagrange_interpolation(x_values, y_values, x):
+    """
+    Расчитывает значение в точке x для интерполирующего многочлена
+    для данной последовательности точек, используя метод Лагранжа.
+
+    Аргументы:
+        x_values (list): список x-координат точек данных.
+        y_values (list): список y-координат точек данных.
+        x (float): точка, в которой вычисляется полином.
+
+    Возвращает:
+        float: значение интерполирующего полинома в точке x.
+    """
+    n = len(x_values)
+    p = 0  # Инициализация интерполирующего полинома
+
+    for i in range(n):
+        L = 1  # Инициализация полинома Лагранжа
+        for j in range(n):
+            if i != j:
+                L *= (x - x_values[j]) / (x_values[i] - x_values[j])
+        p += y_values[i] * L
+
+    return p
+
+
+def lagrange_error(x_values, y_values, x):
+    """
+    Вычислить погрешность в интерполяции Лагранжа.
+
+    Аргументы:
+        x_values (list): список x-координат точек данных.
+        x (float): точка, в которой вычисляется ошибка.
+
+    Возвращает:
+        float: оценка погрешности интерполяции Лагранжа в точке x.
+    """
+    n = len(x_values)
+    product = reduce(operator.mul, [(x - xi) for xi in x_values])
+
+    func = lambda x: lagrange_interpolation(x_values, y_values, x)
+    # значение (n+1)-ой производной в точке x.
+    nth_derivative = derivative(func, x0=x, n=n - 1, order=n + [1, 0][n % 2])
+
+    return nth_derivative * product / factorial(n + 1)
+
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    # Вычисление значений в точках
+    x = [0.43, 0.48, 0.55, 0.62, 0.70, 0.75]
+    y = [1.63597, 1.73234, 1.87686, 2.03345, 2.22846, 2.35973]
+
+    x1 = [0.702, 0.512, 0.645, 0.736]
+    y1 = [lagrange_interpolation(x, y, x1[i]) for i in range(len(x1))]
+    err1 = [lagrange_error(x, y, x1[i]) for i in range(len(x1))]
+
+    x2 = [0.503, 0.441, 0.602, 0.732]
+    y2 = [lagrange_interpolation(x, y, x2[i]) for i in range(len(x2))]
+    err2 = [lagrange_error(x, y, x2[i]) for i in range(len(x2))]
+    print("Метод Лагранжа:")
+    print("|  x1  |  y1  |     err1")
+    print("|------|------| ---------")
+    [print(
+        f"|{x1[i]} |{y1[i]:.3f} | {err1[i]:.8f} ")
+        for i in range(len(x1))]
+    print("\n|  x2  |  y2  |     err2")
+    print("|------|------| ---------")
+    [print(
+        f"|{x2[i]} |{y2[i]:.3f} | {err2[i]:.8f} ")
+        for i in range(len(x1))]
--- lab3/lab3_newton.py
+++ lab3/lab3_newton.py
@@ -1,2 +1,66 @@
+import operator
+import warnings
+from functools import reduce
+from math import factorial
+
+from scipy.misc import derivative
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+def newton_interpolation(x, y, x0):
+    """
+    Вычисляет значение интерполированной функции в точке x0 с использованием второй интерполяционной формулы Ньютона.
+
+    Параметры:
+    x (list): Список точек интерполяции.
+    y (list): Список значений функции в точках интерполяции.
+    x0 (float): Точка, в которой требуется вычислить значение интерполированной функции.
+
+    Возвращает:
+    y0 (float): Значение интерполированной функции в точке x0.
+    """
+    n = len(x)
+
+    # Инициализация разделенной разности
+    fdd = [[0 for x in range(n)]
+            for y in range(n)]
+    y = y.copy()
+
+    # Заполнение нулевой строки таблицы разделенных разностей
+    for i in range(n):
+        fdd[i][0] = y[i]
+
+    # Заполнение других строк таблицы разделенных разностей
+    for i in range(1, n):
+        for j in range(n - i):
+            # Вычисление i-ой разделенной разности
+            fdd[j][i] = (fdd[j + 1][i - 1] - fdd[j][i - 1]) / (x[i + j] - x[j])
+
+    # Вычисление значения интерполированной функции
+    y0 = fdd[0][0]
+    for i in range(1, n):
+        prod = 1
+        for j in range(i):
+            prod *= (x0 - x[j])
+        # Добавление i-ого слагаемого в сумму
+        y0 += prod * fdd[0][i]
+
+    return y0
+
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    # Вычисление значений в точках
+    x = [1.34, 1.345, 1.35, 1.355, 1.36, 1.365, 1.37, 1.375, 1.38, 1.385, 1.39, 1.395]
+    y = [4.25562, 4.35325, 4.45522, 4.56184, 4.67344, 4.79038, 4.91306, 5.04192, 5.17744, 5.32016, 5.47069, 5.62968]
+
+    x1 = [1.3617, 1.3921, 1.3359, 1.4]
+    y1 = [newton_interpolation(x, y, x1[i]) for i in range(len(x1))]
+
+    print("Метод Ньютона (2 формула):")
+    print("|  x1  |  y1 |")
+    print("|------|-----|")
+    [print(
+        f"|{x1[i]:.3f} |{y1[i]:.3f}|")
+        for i in range(len(x1))]
+
